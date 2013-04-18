bd81a59a5d08ba9cc36ecc2c1aa470aba195fcc2
juliusf <julius.flohr@gmail.com>
2013-04-18 10:35:48 +0200

    heavy refactoring, statistics work

--- trafficAnalyzer/RequestBatch.py
+++ trafficAnalyzer/RequestBatch.py
@@ -1,41 +1,42 @@
 class RequestBatch(object):
 	"""docstring for RequestBock"""
-	
-	#_getCount = 0
-	#_dnsCount = 0
-	#_downstreamVolumeBytes = 0
-	#_requestURL = ""
+
 	def __init__(self):
 		self._getCount = 0
 		self._dnsCount = 0
 		self._downstreamVolumeBytes = 0
 		self._requestURL = ""
 
-	def incrementNumberOfGetRequests(self):
-		print self._requestURL
+	def increment_getrequests(self):
 		if self._requestURL != "":
 			self._getCount += 1
 		else:
-			print "WARNING: undefined packet captured!: %s" % (self)
+			print "WARNING: unable to assign packet to RequestBatch"
 			
-
-	def incrementNumberOfDNSRequests(self):
-		global _dnsCount
-		if _ != "":
-			_dnsCount += 1
-		else:
-			print "WARNING: undefined packet captured!"
-
-
-	def increaseDownstreamVolume(self, bytes):
-		global _downstreamVolumeBytes
-		if _requestURL != "":
-			_downstreamVolumeBytes += bytes
+	def get_getrequests(self):
+		return self._getCount
+	
+	def increment_dnsrequests(self):
+		if self._requestURL != "":
+			self._dnsCount += 1
 		else:
 			print "WARNING: unable to assign packet to RequestBatch"
-		
 
-	def setRequestURL(self, requestURL):
+	def get_dnsrequests(self):
+		return self._dnsCount
+
+	def increase_downstreamvolume(self, bytes):
+		if self._requestURL != "":
+			self._downstreamVolumeBytes += bytes
+		else:
+			print "WARNING: unable to assign packet to RequestBatch"
+	
+	def get_downstreamvolume(self):
+		return self._downstreamVolumeBytes	
+
+	def set_requesturl(self, requestURL):
 		self._requestURL = requestURL
-		print "url updated to: %s" % (self._requestURL)
+		#print "url updated to: %s" % (self._requestURL)
 
+	def get_requesturl(self):
+		return self._requestURL
--- trafficAnalyzer/main.py
+++ trafficAnalyzer/main.py
@@ -3,53 +3,86 @@
 from scapy.all import *
 from scapyhttp.HTTP import HTTP
 from RequestBatch import RequestBatch
+import optparse
 
-def updateRequestDomain(domain):
+
+current_packet = None
+request_batch = RequestBatch()
+
+def main():
+    #global vars
+    global current_packet
+    global request_batch
+
+    #commandline option parser
+    parser = optparse.OptionParser()
+    parser.add_option('-p', '--plot', help='creates gnuplot DAT file')
+    (opts, args) = parser.parse_args()
+    
+    packets = utils.rdpcap("13-04-11--18_basic_measurement.pcap")
+    request_batch = RequestBatch()
+
+    for pkt in packets:
+        #packet data extraction
+        current_packet = pkt
+        ipdata = pkt[IP]
+        src =  pkt.getlayer(IP).src
+        dst = pkt.getlayer(IP).dst
+        dport = 0
+        sport = 0
+        l4 = ""
+        if pkt.haslayer(UDP):
+            l4 = "UDP"
+
+        if pkt.haslayer(TCP):
+            l4 = "TCP"
+
+        if l4 == "UDP":
+           dport = pkt.getlayer(UDP).dport
+           sport = pkt.getlayer(UDP).sport
+
+        if l4 == "TCP":
+            dport = pkt.getlayer(TCP).dport
+            sport = pkt.getlayer(TCP).sport
+
+        #check for new url request block:
+
+        if l4 == "UDP" and dport == 1337:
+            update_requestdomain(pkt.getlayer(Raw))
+            
+        else:
+            #HTTP GET extraction
+            if pkt.haslayer(HTTP):
+               extract_http()
+            #DNS Query Count
+            if l4 == "UDP" and dport == 53:
+                request_batch.increment_dnsrequests()
+
+            #DownstreamVolume
+
+            if dst == "10.0.0.23" :
+                request_batch.increase_downstreamvolume(len(pkt.getlayer(IP))) #TODO: w/ or /w ethernet?
+
+
+def update_requestdomain(domain):
     #Update Database here
-    requestBatch = RequestBatch()
-    requestBatch.setRequestURL(domain)
+    global request_batch
+    print '-'*23
+    print "Statistics for: %s" % (request_batch.get_requesturl())
+    print "HTTP GET Requests: %s" % (request_batch.get_getrequests())
+    print "DNS Requests: %s" % (request_batch.get_dnsrequests())
+    print "Downstream Volume: %s" % (request_batch.get_downstreamvolume())
+    request_batch = RequestBatch()
+    request_batch.set_requesturl(domain)
 
-def extractHTTP():
-   httpData = pkt.getlayer(HTTP).payload.fields
+def extract_http():
+    global current_packet
+    global request_batch
+    httpData = current_packet.getlayer(HTTP).payload.fields
 
-   if 'Method' in httpData:
-    requestBatch.incrementNumberOfGetRequests()
-
-
-pkts = utils.rdpcap("13-04-11--18_basic_measurement.pcap")
-
-
-requestBatch = RequestBatch()
+    if 'Method' in httpData:
+        request_batch.increment_getrequests()
 
 
-for pkt in pkts:
-    #packet data extraction
-    ipdata = pkt[IP]
-    src =  pkt.getlayer(IP).src
-    dst = pkt.getlayer(IP).dst
-    dport = 0
-    sport = 0
-    l4 = ""
-    if pkt.haslayer(UDP):
-        l4 = "UDP"
-
-    if pkt.haslayer(TCP):
-        l4 = "TCP"
-
-    if l4 == "UDP":
-       dport = pkt.getlayer(UDP).dport
-       sport = pkt.getlayer(UDP).sport
-
-    if l4 == "TCP":
-        dport = pkt.getlayer(TCP).dport
-        sport = pkt.getlayer(TCP).sport
-
-    #check for new url request block:
-
-    if l4 == "UDP" and dport == 1337:
-        updateRequestDomain(pkt.getlayer(Raw))
-        
-    else:
-        if pkt.haslayer(HTTP):
-           extractHTTP()
-
+if __name__=="__main__":
+   main()
