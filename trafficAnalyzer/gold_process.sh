313276f91419a30505eddbcf45770abb637c5f4f
Julius F <julius.flohr@gmail.com>
2013-06-19 11:11:17 +0200

    removed unnecessary files

--- trafficAnalyzer/gold_process.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-./multi_analyzer.py -f gold/13-06-07--17_desktop_measurement.pcap
-./multi_analyzer.py -f gold/13-06-12--10_desktop_measurement_good_one.pcap
-./multi_analyzer.py -f gold/13-06-15--23_desktop_measurement.pcap
-
-./sqlAnalyzer.py -d -f gold/13-06-07--17_desktop_measurement.pcap.sqlite
-./sqlAnalyzer.py -d -f gold/13-06-12--10_desktop_measurement_good_one.pcap.sqlite
-./sqlAnalyzer.py -d -f gold/13-06-15--23_desktop_measurement.pcap.sqlite
